97ba290b5ccde5dd8eb6150327142372f9ca6f85
Hello-Kishan <corejava27@gmail.com>
2024-12-13 11:27:26 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use Strict" // Treat all js code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+console.log( 3 + 
+    3 );  // code readability should be high
+
+    let name = "kishan"
+    let age = 29
+    let isLoggedIn = false
+
+    // number => 2 to power 53
+    //bigInt
+    //string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined 
+    //sysmbol => unique
+
+    // object
+
+    console.log(typeof undefined); //undefined
+    console.log(typeof null); // object
+    
+    
