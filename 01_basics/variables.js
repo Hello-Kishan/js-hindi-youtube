54a6ef93fb916dee7a607b33eba5a1f497595781
Hello-Kishan <corejava27@gmail.com>
2024-12-13 11:01:05 +0000

    add data types

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 123541
+let accointEmail = "kishan@gmail.com"
+let accountPassword = "5557600"
+let accountCity = "satna"
+
+let accountState = "Madhya Pradesh"
+
+console.log(accountId);
+console.table([accountId, accointEmail, accountCity,accountPassword,accountState]);
+
+
