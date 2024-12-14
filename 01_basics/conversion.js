dc2e56b163987c910dadcbb46ea5106f286bf95b
Hello-Kishan <corejava27@gmail.com>
2024-12-14 05:23:41 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,43 @@
+let score = undefined
+
+// console.log(typeof score);
+//console.log(typeof (score))
+
+let valueInNumber =  Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// null => 0
+//undefined => NaN
+
+
+let isloggedIn = "kishan"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+
+//console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+//"kishan" => true
+
+
+let someNumber = 33
+console.log("before :"+typeof someNumber);
+
+let convertedString =  String(someNumber)
+
+console.log(convertedString);
+console.log("after :"+typeof convertedString);
+
+
+
+
+
+
+
+
